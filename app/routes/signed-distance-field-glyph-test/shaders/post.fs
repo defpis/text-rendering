4d3b579290166c9f9e1993adfe70a2b9d0000d8d
wenjun.super <wenjun.super@bytedance.com>
2025-06-17 17:48:08 +0800

    sdf render

--- app/routes/signed-distance-field-glyph-test/shaders/post.fs
+++ app/routes/signed-distance-field-glyph-test/shaders/post.fs
@@ -9,11 +9,18 @@ uniform sampler2D u_texture1;
 
 out vec4 o_color;
 
+const float t = 0.5;
+
 void main() {
+  // u_texture0 纹理本身由于放大就有锯齿
+  // 采样 u_texture0 得到的 s 因此出现跳变
+  // 进而导致 r 的值不稳定，放大出现锯齿
+  // 生成 sdf 纹理之后再进行线性采样就没有此问题？
   uint n = uint(texture(u_texture0, v_uv).r * 255.0);
   float s = float(n & 1u);
   float d = texture(u_texture1, v_uv).r;
-
   float r = s > 0.0 ? 1.0 - d : d;
-  o_color = vec4(r, 0.0, 0.0, 1.0);
+  float dr = fwidth(r) * 0.5;
+  float a = smoothstep(t - dr, t + dr, r);
+  o_color = vec4(0.0, 0.0, 0.0, a);
 }
